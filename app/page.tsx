d6875f4b7b39b6304a158e8c40b88c34ded246b4
Jakub Figlak <jakub@prograd.uk>
2024-08-13 15:26:33 +0200

    feat: set up shadcn

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,28 @@
+import Image from "next/image";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import prisma from "@/lib/prisma";
 
 const HomePage = async () => {
   const pokemon = await prisma.pokemon.findMany();
 
   return (
-    <main className="flex flex-col items-center justify-center h-screen">
+    <main className="flex flex-col items-center justify-center h-screen gap-6">
       <h1 className="font-bold text-3xl">Poke Collection</h1>
-      <pre>{JSON.stringify(pokemon, null, 2)}</pre>
+      {pokemon.map((pokemon) => (
+        <Card key={pokemon.id}>
+          <CardHeader>
+            <CardTitle className="uppercase text-xl">{pokemon.name}</CardTitle>
+          </CardHeader>
+          <CardContent className="flex justify-center">
+            <Image
+              src={pokemon.imageUrl}
+              alt={pokemon.name}
+              width={96}
+              height={96}
+            />
+          </CardContent>
+        </Card>
+      ))}
     </main>
   );
 };
