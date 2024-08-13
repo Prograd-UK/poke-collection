c811b3621552e323adc64ba655731660be33644d
Jakub Figlak <jakub@prograd.uk>
2024-08-13 16:10:25 +0200

    feat: style list on the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -6,23 +6,29 @@ const HomePage = async () => {
   const pokemon = await prisma.pokemon.findMany();
 
   return (
-    <main className="flex flex-col items-center justify-center h-screen gap-6">
+    <main className="flex flex-col items-center justify-center min-h-screen gap-6">
       <h1 className="font-bold text-3xl">Poke Collection</h1>
-      {pokemon.map((pokemon) => (
-        <Card key={pokemon.id}>
-          <CardHeader>
-            <CardTitle className="uppercase text-xl">{pokemon.name}</CardTitle>
-          </CardHeader>
-          <CardContent className="flex justify-center">
-            <Image
-              src={pokemon.imageUrl}
-              alt={pokemon.name}
-              width={96}
-              height={96}
-            />
-          </CardContent>
-        </Card>
-      ))}
+      <ul className="grid grid-cols-3 gap-4">
+        {pokemon.map((pokemon) => (
+          <li key={pokemon.id}>
+            <Card>
+              <CardHeader>
+                <CardTitle className="uppercase text-xl text-center">
+                  {pokemon.name}
+                </CardTitle>
+              </CardHeader>
+              <CardContent className="flex justify-center">
+                <Image
+                  src={pokemon.imageUrl}
+                  alt={pokemon.name}
+                  width={96}
+                  height={96}
+                />
+              </CardContent>
+            </Card>
+          </li>
+        ))}
+      </ul>
     </main>
   );
 };
